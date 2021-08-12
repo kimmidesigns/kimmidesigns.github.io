f9e2086861d20ae4b5b9e95dc541d6905051380c
bloodandeath <nuctekstaff@gmail.com>
2021-08-12 12:45:33 -0700

    Mobile Update

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -24,11 +24,8 @@ $(document).ready(function(){
   });
   
   // Refresh on resize
-  $(window).bind('resize', function(e) {
-  if (window.RT) clearTimeout(window.RT);
-  window.RT = setTimeout(function() {
-      this.location.reload(false); /* false to get page from cache */
-    }, 100);
+  $(window).resize(function(){
+    location.reload(false);
   });
   
 
